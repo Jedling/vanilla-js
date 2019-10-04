e9575758f58b9d1a7910aa7689d2407e016bb2d3
joel <joel.edling1@gmail.com>
2019-10-04 14:56:07 +0200

    push contact info to array

--- www/index.js
+++ www/index.js
@@ -1,89 +1,99 @@
-// import { unlink } from "fs";
-
-let body = document.querySelector('body');
-let div = document.createElement('div');
+let body = document.querySelector("body");
+let div = document.createElement("div");
 body.append(div);
-
-// Name
-let inputName = document.createElement('input');
-inputName.setAttribute('class', 'input');
-inputName.setAttribute('value', '');
-inputName.setAttribute('autofocus', '');
-inputName.setAttribute('placeholder', 'namn')
+// Name
+let inputName = document.createElement("input");
+inputName.setAttribute("class", "input-field");
+inputName.setAttribute("value", "");
+inputName.setAttribute("id", "name");
+inputName.setAttribute("autofocus", "");
+inputName.setAttribute("placeholder", "namn");
 body.append(inputName);
 div.append(inputName);
-
-// Phone
-let inputPhone = document.createElement('input');
-inputPhone.setAttribute('class', 'input');
-inputPhone.setAttribute('value', '');
-inputPhone.setAttribute('placeholder', 'telefon')
+// Phone
+let inputPhone = document.createElement("input");
+inputPhone.setAttribute("class", "input-field");
+inputPhone.setAttribute("value", "");
+inputPhone.setAttribute("id", "phone");
+inputPhone.setAttribute("placeholder", "telefon");
 body.append(inputPhone);
 div.append(inputPhone);
 
-// Email
-let inputEmail = document.createElement('input');
-inputEmail.setAttribute('class', 'input');
-inputEmail.setAttribute('value', '');
-inputEmail.setAttribute('placeholder', 'e-post')
+// Add Phone number
+let addPhone = document.createElement('button');
+addPhone.setAttribute('class', 'add-btn');
+addPhone.innerHTML = '+';
+div.append(addPhone);
+
+// Email
+let inputEmail = document.createElement("input");
+inputEmail.setAttribute("class", "input-field");
+inputEmail.setAttribute("value", "");
+inputEmail.setAttribute("id", "email");
+inputEmail.setAttribute("placeholder", "e-post");
 body.append(inputEmail);
 div.append(inputEmail);
 
-// Submit
-let submit = document.createElement('button');
-submit.setAttribute('class', 'submit-contact');
-submit.innerHTML = 'Lägg till kontakt';
-// submit.setAttribute('placeholder', 'Lägg Till');
+// Add contact input
+let addEmail = document.createElement('button');
+addEmail.setAttribute('class', 'add-btn');
+addEmail.innerHTML = '+';
+div.append(addEmail);
+
+// Submit
+let submit = document.createElement("button");
+submit.setAttribute("class", "submit-contact");
+submit.innerHTML = "Lägg till kontakt";
 div.append(submit);
 
 let addedContacts = document.createElement("h2");
-addedContacts.setAttribute('class', 'added-contacts');
-addedContacts.innerHTML = 'Osorterad kontaktlista!';
+addedContacts.setAttribute("class", "added-contacts");
+addedContacts.innerHTML = "Osorterad kontaktlista!";
 div.append(addedContacts);
 
-let ul = document.createElement('ul');
-ul.setAttribute('class', 'contact-ul');
+let ul = document.createElement("ul");
+ul.setAttribute("class", "contact-ul");
 div.append(ul);
 
-// Eventhanterare
-
+// Eventhanterare
 
 //////////////////////Object.create!////////////////////////////////////
-
 const userPrototype = {
-  // put methods and fields that have a default value
-  // in the prototype
-    getInput(){
-      window.addEventListener('click', e => {
-  
-        if (e.target.closest('.submit-contact')) {
-          // console.log("klickat på submit");
-          let inputValue = document.querySelectorAll('input[value =""]');
-          inputValue.forEach(val => {
-            let value = val.value;
-            console.log(value);
-            // let contactList = val.value;
-            let list = document.createElement('li');
-            list.setAttribute('class', 'contact-list');
-            list.append(value);
-            ul.append(list);
-          });
-        }
-      });
-    },
+  getInput() {
+    let persons = [];
+    window.addEventListener("click", e => {
+      if (e.target.closest(".submit-contact")) {
+        let inputValues = document.querySelectorAll('input[value =""]');
+        const data = [].reduce.call(
+          inputValues,
+          (acc, cur) => {
+            acc[cur.id] = cur.value;
+            return acc;
+          },
+          {}
+        );
+        console.log(data);
+        persons.push(data)
+        console.log(persons);
+        localStorage.store = JSON.stringify(persons);
+        // for (let [key, val] of Object.entries(data)) {
+        //   let listObj = document.createElement("li");
+        //   listObj.setAttribute("class", "contact-list");
+        //   listObj.append(val);
+        //   ul.append(listObj);
+        // }
+      }
+    });
+  }
 };
+function User(name, email, phone) {
 
-function User (name, email, phone ){
-  // make a new instance/object with
-  // animlPrototype as its prototype
   let newInstance = Object.create(userPrototype);
-  // fields that are different for each instance
-  // need only be assigned here
   newInstance.name = name;
   newInstance.email = email;
   newInstance.phone = phone;
   return newInstance;
-}
-let user = new User()
-console.log(user);
-userPrototype.getInput(new User(this.name));
+};
+
+let user = new User();
+user.getInput();
