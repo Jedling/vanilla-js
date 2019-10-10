621fb23fe26844e3a50fdd73d571386a74334dea
joel <joel.edling1@gmail.com>
2019-10-10 10:01:10 +0200

    created inputs on add phone and id property on contact object

--- www/App.js
+++ www/App.js
@@ -1,24 +1,35 @@
 class App {
   constructor() {
-    
+    // kolla upp diff mellan arrayer ?!
   }
   createDOM() {
     this.listen();
+  
     this.form = new Form();
     this.contacts = new Contacts();
   }
   listen() {
     window.addEventListener("click", e => {
       if (e.target.closest("#save-contact")) this.saveContact();
-      if (e.target.closest("#add-email")) this.addEmail();
+      if (e.target.closest(".add-phone")) this.addPhone().saveContact();
     });
   }
-  addEmail() {
-      console.log('klick add email')
+  addPhone() {
+    //   let input = document.querySelector('#email');
+    //   let added = document.querySelector('div');
+    //   added.append(input)
+    // inputEmail.append(emailValue);
+    // console.log(emailValue);
+    const phoneDiv = document.querySelector("div.phone-div");
+    const input = document.createElement("input");
+    phoneDiv.append(input);
+    let br = document.createElement("br");
+    phoneDiv.append(br);
   }
-  
+
   saveContact() {
     let inputValues = document.querySelectorAll('input[type="text"]');
+
     const data = [].reduce.call(
       inputValues,
       (acc, cur) => {
@@ -27,8 +38,16 @@ class App {
       },
       {}
     );
+  
+    // let id = 0;
     contacts.push(data);
-    console.log(data)
+    contacts.forEach((item, i) => {
+      item.id = i + 1;
+    });
+    // console.log(data);
+    console.log(contacts)
     contacts.save();
+    document.querySelector('div.added-contacts').outerHTML = ''
+    this.contacts = new Contacts()
   }
 }
