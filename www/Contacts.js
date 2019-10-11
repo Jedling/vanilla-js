99f3968d926bf8a1d2435337c4c2819aebbbe785
joel <joel.edling1@gmail.com>
2019-10-11 17:51:41 +0200

    data-attribute test

--- www/Contacts.js
+++ www/Contacts.js
@@ -4,53 +4,63 @@ class Contacts extends App {
     this.addedContacts();
   }
   addedContacts() {
-    let div = document.createElement('div');
-    div.setAttribute('class', 'added-contacts')
+    let div = document.createElement("div");
+    div.setAttribute("class", "added-contacts");
 
-    let body = document.querySelector('body');
+    let body = document.querySelector("body");
     body.append(div);
 
-    let table = document.createElement('table');
-    let thead = document.createElement('thead');
-    let tr = document.createElement('tr');
+    let table = document.createElement("table");
+    let thead = document.createElement("thead");
+    let tr = document.createElement("tr");
 
-    let th = document.createElement('th');
-    th.innerText = 'Namn';
-    let th2 = document.createElement('th');
-    th2.innerText = 'Email';
-    let th3 = document.createElement('th');
-    th3.innerText = 'Telefon';
+    let th = document.createElement("th");
+    th.innerText = "Namn";
+
+    let th2 = document.createElement("th");
+    th2.innerText = "Email";
+    
+    let th3 = document.createElement("th");
+    th3.innerText = "Telefon";
+
+    let th4 = document.createElement("th");
+    th4.innerText = "Redigera";
+
     tr.append(th);
     tr.append(th2);
     tr.append(th3);
+    tr.append(th4);
     thead.append(tr);
     table.append(thead);
 
-    let tbody = document.createElement('tbody');
+    let tbody = document.createElement("tbody");
 
     contacts.forEach(contact => {
-      let tr = document.createElement('tr');
-      let td = document.createElement('td');
-      td.innerText = contact.name;
+      let tr = document.createElement("tr");
+      let td = document.createElement("td");
+      tr.setAttribute('data-contactid', contact.id);  
+      td.innerText = contact.name ;
 
-      let td2 = document.createElement('td');
-      td2.innerText = contact.email.join('\n');
+      let td2 = document.createElement("td");
+      td2.innerText = contact.email.join("\n");
 
-      let td3 = document.createElement('td');
-      td3.innerText = contact.phone.join('\n');
+      let td3 = document.createElement("td");
+      td3.innerText = contact.phone.join("\n");
 
+      let td4 = document.createElement("td");
+      let edit = document.createElement("button");
+      edit.setAttribute("class", "edit");
+      edit.setAttribute("value", "");
+      edit.innerHTML = "Redigera";
+
+      td4.append(edit);
       tr.append(td);
       tr.append(td2);
       tr.append(td3);
+      tr.append(td4);
       tbody.append(tr);
-      
     });
     table.append(tbody);
     div.append(table);
   }
 }
-
-// let addedContacts = document.createElement("h2");
-//     addedContacts.setAttribute("class", "added-contacts");
-//     addedContacts.innerHTML = "Osorterad kontaktlista!";
-//     div.append(addedContacts);
