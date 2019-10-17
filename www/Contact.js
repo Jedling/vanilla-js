8a75f86af2a6b6683b5274e26d802c4b2a225106
joel <joel.edling1@gmail.com>
2019-10-17 23:05:42 +0200

    go back html and forEach now with index in showHistory()

--- www/Contact.js
+++ www/Contact.js
@@ -4,17 +4,15 @@ class Contact {
     this.showContact();
     this.showHistory();
   }
+
   showContact() {
     let contact = contacts.find(contact => {
-      
       return contact.id === this.id;
-
     });
-    console.log(this.id)
+    
     let version = contact.history[contact.pointer];
-    console.log(version);
 
-    let lastOne = document.querySelector('.added-contact');
+    let lastOne = document.querySelector(".added-contact");
     lastOne && lastOne.remove();
 
     let div = document.createElement("div");
@@ -27,6 +25,13 @@ class Contact {
     h2.setAttribute("class", "history-header");
     h2.innerText = "Nuvarande kontaktuppgifter";
     div.append(h2);
+
+    let goBackButton = document.createElement("button");
+    goBackButton.setAttribute("class", "back");
+    goBackButton.setAttribute("data-contactid", contact.id);
+    goBackButton.setAttribute("type", "button");
+    goBackButton.innerText = "Gå Tillbaka";
+    div.append(goBackButton);
 
     let table = document.createElement("table");
     let thead = document.createElement("thead");
@@ -81,12 +86,12 @@ class Contact {
     table.append(tbody);
     div.append(table);
   }
+  
   showHistory() {
     let contact = contacts.find(contact => {
       return contact.id === this.id;
-    }).history
-
-    let lastOne = document.querySelector('.history');
+    }).history;
+    let lastOne = document.querySelector(".history");
     lastOne && lastOne.remove();
 
     let div = document.createElement("div");
@@ -110,33 +115,35 @@ class Contact {
 
     let th = document.createElement("th");
     th.setAttribute("class", "history-th");
-    th.innerText = "Telefon";
+    th.innerText = "Namn";
 
     let th2 = document.createElement("th");
-    th2.innerText = "Email";
+    th2.innerText = "Epost";
 
     let th3 = document.createElement("th");
     th3.innerText = "Telefon";
 
-    // let th4 = document.createElement("th");
-    // th4.innerText = "Redigera";
+    let th4 = document.createElement("th");
+    th4.innerText = "Redigerad";
+
+    let th5 = document.createElement("th");
+    th5.innerText = "Återställ";
 
     tr.append(th);
     tr.append(th2);
     tr.append(th3);
-    // tr.append(th4);
-
+    tr.append(th4);
+    tr.append(th5);
     thead.append(tr);
     table.append(thead);
 
     let tbody = document.createElement("tbody");
 
     // contact loop
-    contact.forEach(contact => {
+    contact.forEach((contact, i) => {
       let tr = document.createElement("tr");
-      tr.setAttribute('class', 'undo-redo')
+      tr.setAttribute("class", "undo-redo");
       let td = document.createElement("td");
-      console.log(contact)
       td.innerText = contact.name;
 
       let td2 = document.createElement("td");
@@ -145,9 +152,26 @@ class Contact {
       let td3 = document.createElement("td");
       td3.innerText = contact.phone.join("\n");
 
+      let td4 = document.createElement("td");
+      td4.setAttribute('class', 'time')
+      td4.innerText = contact.time;
+
+      let td5 = document.createElement("td");
+      let reset = document.createElement("button");
+      reset.setAttribute("class", "reset");
+      // reset.setAttribute('data-index', i);
+      // reset.setAttribute("data-contactid", contact.id);
+      reset.setAttribute('data-index', i)
+      reset.setAttribute('data-id', this.id)
+      reset.setAttribute("value", "");
+      reset.innerHTML = "Återställare";
+      td5.append(reset);
+
       tr.append(td);
       tr.append(td2);
       tr.append(td3);
+      tr.append(td4);
+      tr.append(td5)
       tbody.append(tr);
     });
     table.append(tbody);
