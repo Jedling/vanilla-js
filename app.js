e93f6f12e68cd9e5a7333fc2cd60130e4a07bd93
joel <joel.edling1@gmail.com>
2019-10-17 23:04:15 +0200

    no change done

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ const app = express();
 // Tell the web server to serve files
 // from the www folder
 app.use(express.static('www'));
+
 // Start the web server on port 3000
 app.listen(3000,() => console.log('Listening on port 3000'));
